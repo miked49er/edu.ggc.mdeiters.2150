668ffb5570f63f6dc2f2adba58e24a8ee8d6d304
miked49er <miked49er@gmail.com>
2015-02-17 21:18:23 -0500

    Card Class and InvalidCardException

---
+++ edu.ggc.mdeiters.HomeWork/src/edu/ggc/mdeiters/HomeWork2/InvalidCardException.java
@@ -0,0 +1,26 @@
+package edu.ggc.mdeiters.HomeWork2;
+
+
+/** Class: InvalidCardException
+ * @author Mike Deiters
+ * @version 1.0
+ * ITEC 2150 Spring 2015
+ * Written: Feb 17, 2015
+ * 
+ * 
+ * This will create a new exception for invalid card values or suits
+ * 
+ * Purpose is create a new exception
+ * 
+ */
+public class InvalidCardException extends Exception {
+
+	/**
+	 * @param message
+	 */
+	public InvalidCardException(String message) {
+
+		super(message);
+	}
+
+}
