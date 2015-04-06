1d3826cf621b7daec0c56b656e2aa44de1e29cfe
miked49er <miked49er@gmail.com>
2015-04-06 14:06:37 -0400

    HomeWork 3

--- edu.ggc.mdeiters.HomeWork/src/edu/ggc/mdeiters/HomeWork3/ReverseRec.java
+++ edu.ggc.mdeiters.HomeWork/src/edu/ggc/mdeiters/HomeWork3/ReverseRec.java
@@ -11,23 +11,31 @@ import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
  * ITEC 2150 Spring 2015
  * Written: Mar 31, 2015
  * 
- * Class Description: TODO
+ * Class Description: Reversing strings
  * 
- * Purpose: TODO
+ * Purpose: use recursion to reverse strings
  * 
  */
 public class ReverseRec {
 
 	private String orig;
-	private String rev;
+	private String wordEnds;
+	private String revEnds;
 	private ArrayList<String> revList;
 	private ArrayList<String> revWordList;
 
+	/**
+	 * Constructor: ReverseRec
+	 * 
+	 */
 	public ReverseRec() {
 
 		this.orig = "";
+		this.wordEnds = "";
+		this.revEnds = "";
 		this.revList = new ArrayList<String>();
 		this.revWordList = new ArrayList<String>();
+
 	}
 
 	/**
@@ -38,9 +46,19 @@ public class ReverseRec {
 	 */
 	public void reverseString(String str) {
 
+		// Assigns str to orig
+
 		this.orig = str;
 		buildList(str);
 		pullStrings(this.revList);
+
+		// Assigns the return of lastChar to wordEnds
+
+		this.wordEnds = lastChar(this.revWordList, 0);
+
+		// Assigns the return of reverseWord to revEnds
+
+		this.revEnds = reverseWords(this.wordEnds);
 
 	}
 
@@ -52,81 +70,136 @@ public class ReverseRec {
 	 */
 	private String buildList(String str) {
 
+		// Assigns the first word of the string to orig
+
 		String orig = str.substring(str.lastIndexOf(" ") + 1);
+
+		// Adds orig to revList
 
 		this.revList.add(orig);
 
-		if (str.length() - orig.length() - 1 < 0) {
+		if (str.length() - orig.length() - 1 < 0) { // If str - orig -1 is less than 0 then return str
+
 			return str;
 		}
-		else {
+		else { // Otherwise call the method again
+
 			buildList(str.substring(0, str.length() - orig.length() - 1));
 		}
 
 		return orig;
 	}
 
+	/**
+	 * Method: pullStrings 
+	 * @param list ArrayList of Strings
+	 * @return rev String
+	 * Method Description: Recursively pulls strings from the list, passes them to the reverseWords method, and adds the return of reverseWord to revWordList
+	 */
 	private String pullStrings(ArrayList<String> list) {
 
 		String rev = "";
-		if (list.isEmpty()) {
+		if (list.isEmpty()) { // Returns an empty string if list is empty
 
 			return "";
 		}
-		if (!list.isEmpty()) {
+		if (!list.isEmpty()) { // If list is not empty the assign the first element to rev and remove the element from list
 
 			rev = list.get(0);
 			list.remove(0);
 		}
+
+		// Adds the return of reverseWords method to revWordList
 
 		this.revWordList.add(reverseWords(rev));
 		pullStrings(list);
 		return rev;
 	}
 
+	/**
+	 * Method: reverseWords 
+	 * @param str String to be reversed
+	 * @return rev a reversed string
+	 * Method Description: Recursively reverses the string
+	 */
 	private String reverseWords(String str) {
 
-		if (str.length() - 1 == 0) {
+		if (str.length() - 1 == 0) { // If the string is a single char then return the char
 
 			return str;
 		}
+
+		// Assigns the recursively return of reverseWords to rev
 
 		String rev = reverseWords(str.substring(1)) + str.substring(0, 1);
 
 		return rev;
 	}
 
-	private String lastChar(String str) {
+	/**
+	 * Method: lastChar 
+	 * @param str ArrayList of Strings
+	 * @param index int of the location to be in str
+	 * @return String of the last char in each element of str
+	 * Method Description: Recursively finds the last char in each element of str to return
+	 */
+	private String lastChar(ArrayList<String> str, int index) {
 
-		if (str.isEmpty()) {
+		String rev = "";
+
+		if (str.size() <= index) { // If str is smaller than index return an empty string
 
 			return "";
 		}
-		if (str.length() >= 0) {
+		else { // Otherwise assign the last char of str to rev
 
-			return str.substring(str.length() - 1);
+			rev += str.get(index).substring(str.get(index).length() - 1);
 		}
 
-		String rev = lastChar(str.substring(str.length() - 2));
+		if (str.size() > index) { // If str is larger than index then call lastChar again
+
+			rev += lastChar(str, index + 1);
+		}
 		return rev;
 	}
 
 	@Override
+	/**
+	 * Method: toString
+	 * @return String
+	 * Method Description: Output the original string along with the reversed, last chars, and the reverse of the last chars
+	 */
 	public String toString() {
 
 		String str = "";
 
-		str += this.orig + "\n";
+		// The output of orig
 		
-		for (int i = 0; i < revWordList.size(); i++) {
+		str += this.orig + "\n";
 
-			if (i == revWordList.size() - 1) {
+		for (int i = 0; i < revWordList.size(); i++) { // Loop through to output all of revWordList as a String
+
+			if (i == revWordList.size() - 1) { // If the last element of revWordList then add a new line char to the string
+				
 				str += revWordList.get(i) + "\n";
 			}
-			else {
+			else { // Otherwise just output the element
+				
 				str += revWordList.get(i) + " ";
 			}
 		}
+
+		// Output revWordList as a list
+		
+		str += revWordList + "\n";
+
+		// Output wordEnds
+		
+		str += this.wordEnds + "\n";
+		
+		// Output revEnds
+		
+		str += this.revEnds;
 
 		return str;
 	}
--- edu.ggc.mdeiters.HomeWork/src/edu/ggc/mdeiters/HomeWork3/ReverseRecTester.java
+++ edu.ggc.mdeiters.HomeWork/src/edu/ggc/mdeiters/HomeWork3/ReverseRecTester.java
@@ -1,15 +1,17 @@
 
 package edu.ggc.mdeiters.HomeWork3;
 
+import java.util.ArrayList;
+
 /** Class: ReverseRecTester
  * @author Mike Deiters
  * @version 1.0
  * ITEC 2150 Spring 2015
  * Written: Mar 31, 2015
  * 
- * Class Description: TODO
+ * Class Description: Tester of ReverseRec class
  * 
- * Purpose: TODO
+ * Purpose: Send a string into ReverseRec to be reversed
  * 
  */
 public class ReverseRecTester {
@@ -17,10 +19,11 @@ public class ReverseRecTester {
 	public static void main(String args[]) {
 
 		ReverseRec rr = new ReverseRec();
-		String test = "This is a test of the call";
 		
-//		System.out.println(rr.reverseWords("Test"));
+		// Test String
 		
+		String test = "This is a test of the reversal call";
+
 		rr.reverseString(test);
 		System.out.println(rr.toString());
 	}
